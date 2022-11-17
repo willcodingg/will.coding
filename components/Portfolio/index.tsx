ae03b8a01af12a95b50890cb4d87ceaceb0a2f51
William Brito <william.brito@descomplica.com.br>
2022-11-17 09:54:30 -0300

    feat: portfolio change

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -2,16 +2,16 @@ import { Typography } from "../Typography";
 
 export function Portfolio() {
   return (
-    <div className="flex justify-around items-center flex-col max-w-sm p-5 pb-0 gap-12 font-bold min-h-screen">
+    <div className="flex justify-around items-center flex-col max-w-sm p-5 pb-0 gap-12 min-h-screen">
       <Typography className="font-bold pt-8 text-3xl">Portfolio</Typography>
-      <div className="bg-brand-secondary pt-12 pb-12 pl-24 pr-24 rounded-lg drop-shadow-xl">
-        <Typography className="text-2xl ">SOON</Typography>
-      </div>
-      <div className="bg-brand-secondary pt-12 pb-12 pl-24 pr-24 rounded-lg drop-shadow-xl">
+      <div className="border-white border-solid border-2 pt-12 pb-12 pl-24 pr-24 rounded-lg drop-shadow-xl">
         <Typography className="text-2xl">SOON</Typography>
       </div>
-      <div className="bg-brand-secondary pt-12 pb-12 pl-24 pr-24 rounded-lg drop-shadow-xl">
-        <Typography className="text-2xl ">SOON</Typography>
+      <div className="border-brand-secondary border-solid border-2 pt-12 pb-12 pl-24 pr-24 rounded-lg drop-shadow-xl">
+        <Typography className="text-2xl">SOON</Typography>
+      </div>
+      <div className="border-white border-solid border-2 pt-12 pb-12 pl-24 pr-24 rounded-lg drop-shadow-xl">
+        <Typography className="text-2xl">SOON</Typography>
       </div>
     </div>
   );
