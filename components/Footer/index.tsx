99c7c9fcdce9c0485e9456747bcc655ad3ad12bf
William Brito <william.brito@descomplica.com.br>
2022-11-20 22:06:33 -0300

    fix

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,38 @@
 import { Typography } from "../Typography";
+import { BsInstagram } from "react-icons/bs";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 export function Footer() {
   return (
-    <div className="flex flex-col items-center font-bold text-sm pt-8 pb-8">
-      <Typography>@2022 willcoding.com.br</Typography>
-      <Typography>
-        <span className="text-brand-secondary">Powered by</span> will.coding
+    <div>
+      <section className="flex text-3xl gap-12 justify-center dark:text-white">
+        <a
+          className="cursor-pointer hover:text-brand-secondary"
+          href="https://www.github.com/willcodingg/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FiGithub />
+        </a>
+        <a
+          className="cursor-pointer hover:text-brand-secondary"
+          href="https://www.instagram.com/will.coding/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+        <a
+          className="cursor-pointer hover:text-brand-secondary"
+          href="https://www.linkedin.com/in/willcoding/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FiLinkedin />
+        </a>
+      </section>
+      <Typography className="text-center pt-4">
+        Build by <span className="text-brand-secondary">William Jonathan</span>
       </Typography>
     </div>
   );
--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -1,6 +1,5 @@
 import { Typography } from "../Typography";
-import { BsInstagram } from "react-icons/bs";
-import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { Footer } from "../Footer";
 
 export function Portfolio() {
   return (
@@ -12,38 +11,7 @@ export function Portfolio() {
       <div className="border-brand-lighter text-brand-lighter border-solid border-2 pt-14 pb-14 lg:pb-24 lg:pt-24">
         <Typography className="text-5xl text-center">SOON</Typography>
       </div>
-      <div>
-        <section className="flex text-3xl gap-12 justify-center dark:text-white">
-          <a
-            className="cursor-pointer hover:text-brand-secondary"
-            href="https://www.github.com/willcodingg/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FiGithub />
-          </a>
-          <a
-            className="cursor-pointer hover:text-brand-secondary"
-            href="https://www.instagram.com/will.coding/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BsInstagram />
-          </a>
-          <a
-            className="cursor-pointer hover:text-brand-secondary"
-            href="https://www.linkedin.com/in/willcoding/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FiLinkedin />
-          </a>
-        </section>
-        <Typography className="text-center pt-4">
-          Build by{" "}
-          <span className="text-brand-secondary">William Jonathan</span>
-        </Typography>
-      </div>
+      <Footer />
     </div>
   );
 }
